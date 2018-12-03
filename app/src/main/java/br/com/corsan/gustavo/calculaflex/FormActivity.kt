c45044e7d414320d34a409a2e4cfcf0044003973
gustavocorreia <gustavocorreiasantos@outlook.com>
2018-12-03 21:45:12 -0200

    2° Aula

--- app/src/main/java/br/com/corsan/gustavo/calculaflex/FormActivity.kt
+++ app/src/main/java/br/com/corsan/gustavo/calculaflex/FormActivity.kt
@@ -1,8 +1,10 @@
 package br.com.corsan.gustavo.calculaflex
 
+import br.com.corsan.gustavo.calculaflex.extensions.getDouble
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.corsan.gustavo.calculaflex.watches.DecimalTextWatcher
 import kotlinx.android.synthetic.main.activity_form.*
 
 class FormActivity : AppCompatActivity() {
@@ -11,12 +13,15 @@ class FormActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
 
+        etGasAverage.addTextChangedListener(DecimalTextWatcher(etGasAverage))
+        etEthanolAverage.addTextChangedListener(DecimalTextWatcher(etEthanolAverage, 1))
+
         btCalculate.setOnClickListener(){
             val nextView = Intent(this,ResultActivity::class.java)
-            nextView.putExtra( "GAS_PRICE",etGasPrice.text.toString())
-            nextView.putExtra("ETHANOL_PRICE",etEthanolPrice.text.toString())
-            nextView.putExtra("GAS_AVERAGE",etGasAverage.text.toString())
-            nextView.putExtra("ETHANOL_AVERAGE",etEthanolAverage.text.toString())
+            nextView.putExtra( "GAS_PRICE",etGasPrice.getDouble())
+            nextView.putExtra("ETHANOL_PRICE",etEthanolPrice.getDouble())
+            nextView.putExtra("GAS_AVERAGE",etGasAverage.getDouble())
+            nextView.putExtra("ETHANOL_AVERAGE",etEthanolAverage.getDouble())
             startActivity(nextView)
         }
 
--- app/src/main/java/br/com/corsan/gustavo/calculaflex/ResultActivity.kt
+++ app/src/main/java/br/com/corsan/gustavo/calculaflex/ResultActivity.kt
@@ -1,12 +1,49 @@
 package br.com.corsan.gustavo.calculaflex
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.corsan.gustavo.calculaflex.extensions.format
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        calculate()
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calculate() {
+        val gasPrice = intent.extras.getDouble("GAS_PRICE", 0.0)
+        val ethanolPrice = intent.extras.getDouble("ETHANOL_PRICE", 0.0)
+        val gasAverage = intent.extras.getDouble("GAS_AVERAGE",0.0)
+        val ethanolAverage = intent.extras.getDouble("ETHANOL_AVERAGE",0.0)
+
+        val perfomanceMyCar = ethanolAverage / gasAverage
+        val priceIndex = ethanolPrice / gasPrice
+
+        if(priceIndex <= perfomanceMyCar){
+            tvSuggestion.text = getString(R.string.label_ethanol)
+        } else{
+            tvSuggestion.text = getString(R.string.label_gasoline)
+        }
+
+        tvGasAverageResult.text = (gasPrice / gasAverage).format(4)
+        tvEthanolAverageResult.text = (ethanolPrice / ethanolAverage).format(4)
+
+        tvFuelRatio.text = getString(R.string.label_ratio_fuel, perfomanceMyCar.format(2))
+    }
+
+
 }
